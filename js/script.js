2ad5e659b61a974f0bd523b2ff66b75c7bcf0b8c
sadyych <147981611+sadyych@users.noreply.github.com>
2024-05-31 10:08:02 +0300

    the end

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@ var logo_white = document.querySelector(".header__logo-white");
 var reg_button = document.querySelector(".header__reg");
 var sign_button = document.querySelector(".header__sign");
 var header_buttons = document.querySelector(".header__btns");
+var main_screen = document.querySelector(".main-page");
+var private_jet = document.querySelector(".private-jet");
+var specification = document.querySelector(".specification");
+var lux_charters = document.querySelector(".luxury-charters");
+var membership = document.querySelector(".membership");
+var footer = document.querySelector(".footer");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
@@ -17,11 +23,23 @@ burger.addEventListener("click", () => {
   reg_button.classList.toggle("active");
   sign_button.classList.toggle("active");
   header_buttons.classList.toggle("active");
+  main_screen.classList.toggle("active");
+  private_jet.classList.toggle("active");
+  specification.classList.toggle("active");
+  lux_charters.classList.toggle("active");
+  membership.classList.toggle("active");
+  footer.classList.toggle("active");
 });
 var link = document.querySelectorAll(".header__link");
 
 link.forEach(function (link) {
   link.addEventListener("click", function (event) {
+    main_screen.classList.remove("active");
+    private_jet.classList.remove("active");
+    specification.classList.remove("active");
+    lux_charters.classList.remove("active");
+    membership.classList.remove("active");
+    footer.classList.remove("active");
     burger.classList.remove("active");
     menu.classList.remove("active");
     body.classList.remove("lock");
@@ -32,6 +50,7 @@ link.forEach(function (link) {
     header_buttons.classList.remove("active");
   });
 });
+
 //-------------------Planes-Swiper--------------------------
 const planesSwiper = new Swiper(".planes-slider", {
   centeredSlides: true,
@@ -56,33 +75,30 @@ const planesSwiper = new Swiper(".planes-slider", {
   },
 });
 const membershipSwiper = new Swiper(".membership-slider", {
-  loop: true,
   centeredSlides: true,
   initialSlide: 1,
   watchSlidesProgress: true,
-  slidesPerView: 3,
-  loopedSlides: 3,
-
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+    500: {
+      spaceBetween: 15,
+      slidesPerView: 3,
+    },
+  },
+  loop: true,
   autoplay: {
     delay: 1500,
     stopOnLastSlide: false,
     disableOnInteraction: false,
   },
   speed: 1000,
-  breakpoints: {
-    320: {
-      // slidesPerView: 1,
-      spaceBetween: 50,
-    },
-    500: {
-      spaceBetween: 24,
-    },
-  },
-  //   spaceBetween: 50,
   effect: "coverflow",
   coverflowEffect: {
     rotate: 0,
-    stretch: 0,
+    stretch: -40,
     depth: 100,
     slideShadows: false,
   },
